2f8754208a7106816fe433305ffe35262f2a0a7c
ArrowMinded <13tiara@gmail.com>
2015-09-07 17:32:17 +0300

    added some languages

--- src/main/java/com/teo/neighborhood/Language.java
+++ src/main/java/com/teo/neighborhood/Language.java
@@ -7,5 +7,5 @@ package com.teo.neighborhood;
 public enum Language {
 	// All Languages in this planet
 	//If you have baby-as a language- why have none as a language???
-	BULGARIAN, ENGLISH, FRENCH, ITALIAN, DEUTSCH, DRUNK, BABY, NONE, JAPANESE, MANDARIN, ARABIC
+	BULGARIAN, ENGLISH, FRENCH, ITALIAN, DEUTSCH, DRUNK, BABY, NONE, JAPANESE, MANDARIN, ARABIC, SWEDISH
 }
